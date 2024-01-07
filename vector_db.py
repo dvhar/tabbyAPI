3e8705d0e916e0eff7e1599026a666dcd20ff659
dvhar <dh@protonmail.com>
2024-01-07 10:16:34 -0600

    add persistence for vector db

--- vector_db.py
+++ vector_db.py
@@ -1,34 +1,40 @@
 import chromadb
 from chromadb.config import Settings
 import itertools
-from fastapi import UploadFile
+from fastapi import FastAPI, UploadFile
 
-chroma_client = chromadb.Client(Settings(anonymized_telemetry=False))
+chroma_client = chromadb.PersistentClient(
+        settings=Settings(anonymized_telemetry=False),
+        path='/tmp/chroma_data')
 chroma_collection = chroma_client.get_or_create_collection("alldocs")
 
 def group_consecutive(ids: list, docs: list):
     result = []
-    sublist = []
-    sublistids = []
+    sublist_texts = []
+    sublist_ids = []
     for i, id in enumerate([int(id) for id in ids]):
-        if not sublistids or id - sublistids[-1] == 1:
-            sublist.append(docs[i])
-            sublistids.append(id)
+        if not sublist_ids or id - sublist_ids[-1] == 1:
+            sublist_texts.append(docs[i])
+            sublist_ids.append(id)
         else:
-            result.append('.'.join(sublist))
-            sublist = [docs[i]]
-            sublistids = [id]
-    result.append('.'.join(sublist))
+            result.append('.'.join(sublist_texts))
+            sublist_texts = [docs[i]]
+            sublist_ids = [id]
+    result.append('.'.join(sublist_texts))
     return result
 
 def index(file: UploadFile):
     """Index a text file with chromadb"""
     global chroma_collection
-    chroma_collection = chroma_client.get_or_create_collection(file.filename)
+    try:
+        chroma_collection = chroma_client.create_collection(file.filename)
+    except:
+        chroma_collection = chroma_client.get_collection(file.filename)
+        return {'message': 'Already indexed'}
     data = [sentence for sentence in file.file.read().decode('utf-8').split('.')]
     ids=[str(i) for i, _ in enumerate(data)]
     chroma_collection.upsert(documents=data, ids=ids)
-    return {'message': 'Document indexed.'}
+    return {'message': 'Document indexed'}
 
 def search(query: str):
     """Return relavant texts from chromadb"""
@@ -46,3 +52,23 @@ def search(query: str):
     docs = moretext.get('documents')
     groups = group_consecutive(ids, docs)
     return {'result': groups}
+
+# for testing vector database endpoints by themselves
+if __name__ == '__main__':
+    import uvicorn
+    app = FastAPI()
+
+    @app.post("/v1/chroma/index")
+    async def index_(file: UploadFile):
+        return index(file)
+
+    @app.get("/v1/chroma/search")
+    async def search_(query: str):
+        return search(query)
+
+    uvicorn.run(
+        app,
+        host='0.0.0.0',
+        port=6000,
+        log_level="debug",
+    )
